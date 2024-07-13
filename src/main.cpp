3a227698781c1fbfa90bcd510623df48ba531214
Hammad Majid <hammadmajid@proton.me>
2024-07-13 22:16:27 +0500

    Implement basic timer logic in a different thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <thread>
 
-int main() {
-    return EXIT_SUCCESS;
+unsigned int remaining_seconds;
+
+void timer() {
+  using namespace std::chrono_literals;
+
+  while (remaining_seconds != 0) {
+    std::cout << remaining_seconds << "\n";
+    remaining_seconds--;
+    std::this_thread::sleep_for(1s);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage:\n\ttimer <seconds>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  remaining_seconds = abs(std::stoi(argv[1]));
+
+  std::thread worker(timer);
+
+  worker.join();
+
+  return EXIT_SUCCESS;
 }
